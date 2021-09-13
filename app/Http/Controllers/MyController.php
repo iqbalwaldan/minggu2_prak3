e1ba3cadb6a5aed7cbef6aef164d9078bc0ff4f8
iqbalwaldan <iqbalwaldan0510@gmail.com>
2021-09-13 22:17:26 +0700

    Membuat Menu Utama

---
+++ app/Http/Controllers/MyController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyController extends Controller
+{
+    public function index(){
+        return 'Selamat Datang Di Webside Jurusan Teknologi Informasi Politeknik Negeri Malang';
+    }
+}
